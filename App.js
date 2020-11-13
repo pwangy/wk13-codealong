9d3b4d73ef1da61afca2392b41a289f9379ded47
Peggy <64912576+pwangy@users.noreply.github.com>
2020-11-13 16:56:00 +0100

    save

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/native'
 
+import { Container } from './components/Container'
 import { Houses } from './components/Houses'
-
-const Container = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
-  align-items: center;
-`
-const Title = styled.Text`
-  font-size 24px;
-  color: palevioletred;
-`
 
 export default function App() {
   return (
--- components/Houses.js
+++ components/Houses.js
@@ -1,22 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import styled from 'styled-components/native'
 
-const Container = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
-  align-items: center;
-`
-const Title = styled.Text`
-  font-size 24px;
-  color: palevioletred;
-`
+import { Container } from './Container'
 
 export const Houses = () => {
   const [houses, setHouses] = useState([])
+  const housesApi = 'https://www.anapioficeandfire.com/api/houses'
 
   useEffect(() => {
-    const housesApi = 'https://www.anapioficeandfire.com/api/houses'
     fetch(housesApi)
       .then((res) => res.json())
       .then((json) => setHouses(json))
